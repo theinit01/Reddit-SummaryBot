743a0c19b022b180e9e5e7b340fdbc8b49cdef51
theinit01 <tushar3145@gmail.com>
2024-05-09 18:28:22 +0530

    Added functionality to avoid repeated comments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from llmsummary import summarize
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import json
 
 
 # Load environment variables from .env file
@@ -23,15 +24,30 @@ def getTextFromURL(url):
     text = ' '.join(map(lambda p: p.text, soup.find_all('p')))
     return text
 
+def load_posted_summaries():
+    try:
+        with open('posted_summaries.json', 'r') as file:
+            return set(json.load(file))
+    except FileNotFoundError as e:
+        print(e)
+        return set()
+
+def save_posted_summaries(posted_summaries):
+    with open('posted_summaries.json', 'w') as file:
+        json.dump(list(posted_summaries), file)
 
 reddit_instance  = praw.Reddit(client_id=client_id, client_secret=client_secret, username=username, password=password, user_agent="Sumarizer-Bot")
 
 subreddit = reddit_instance.subreddit("india")
-top_posts = subreddit.rising(limit=3)
+top_posts = subreddit.rising(limit=7)
 
+posted_summaries = load_posted_summaries()
 
 for post in top_posts:
     if not post.is_self:
+        if post.id in posted_summaries:
+            continue
+        
         url = post.url
         article = getTextFromURL(url)
         summary = summarize(article)
@@ -39,3 +55,6 @@ for post in top_posts:
         if "This is not an article." not in summary:
             post.reply("Here is a summary of the article: \n\n" + summary)
             print(f"Posted summary for post: {post.title}")
+            posted_summaries.add(post.id)
+
+save_posted_summaries(posted_summaries)
