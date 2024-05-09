67f170ab0114164280b2accd7e7bf5681f038dbd
theinit01 <tushar3145@gmail.com>
2024-05-09 18:31:18 +0530

    later modifications

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ for post in top_posts:
         summary = summarize(article)
         
         if "This is not an article." not in summary:
-            post.reply("Here is a summary of the article: \n\n" + summary)
+            #post.reply("Here is a summary of the article: \n\n" + summary)
             print(f"Posted summary for post: {post.title}")
             posted_summaries.add(post.id)
 
